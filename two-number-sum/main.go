0df6e382c6ea62e622b50c10569937e621a4a7bd
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-17 19:58:23 +0200

    [Two number sum] First solution.

--- two-number-sum/main.go
+++ two-number-sum/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func nestedLoopSolution(array []int, targetSum int) []int {
+  for i := 0; i < len(array); i++ {
+    firstNum := array[i]
+    for j := i+1; j < len(array); j++ {
+      secondNum := array[j]
+      if firstNum + secondNum == targetSum {
+        return []int{firstNum, secondNum}
+      }
+    }
+  }
+  return []int{}
+}
+
 func main() {
-  fmt.Println("Hello two number sum!")
+  input := []int{3,5,-4,8,11,1,-1,6}
+  fmt.Printf("First solution's result => %d\n", nestedLoopSolution(input, 10))
 }
