085ca1b42534d0969084f5cf8c2d978700a8af94
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-17 20:21:31 +0200

    [Two number sum] Second solution.

--- two-number-sum/main.go
+++ two-number-sum/main.go
@@ -15,7 +15,20 @@ func nestedLoopSolution(array []int, targetSum int) []int {
   return []int{}
 }
 
+func hashMapSolution(array []int, targetSum int) []int {
+	nums := map[int]bool{}
+	for _, num := range array {
+		possibleMatch := targetSum - num
+		if _, found := nums[possibleMatch]; found {
+			return []int{num, possibleMatch}
+		}
+		nums[num] = true
+	}
+	return []int{}
+}
+
 func main() {
   input := []int{3,5,-4,8,11,1,-1,6}
   fmt.Printf("First solution's result => %d\n", nestedLoopSolution(input, 10))
+  fmt.Printf("Second solution's result => %d\n", hashMapSolution(input, 10))
 }
