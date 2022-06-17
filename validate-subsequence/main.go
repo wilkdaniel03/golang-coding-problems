59a8856a74825c145beeb688f5507d6316a485b3
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-17 20:35:25 +0200

    [Validate subsequence] Second solution.

--- validate-subsequence/main.go
+++ validate-subsequence/main.go
@@ -13,8 +13,22 @@ func firstSolution(array []int, sequence []int) bool {
 	return sequenceIdx == len(sequence)
 }
 
+func secondSolution(array []int, sequence []int) bool {
+	sequenceIdx := 0
+	for _, num := range array {
+		if sequenceIdx == len(sequence) {
+			break
+		}
+		if num == sequence[sequenceIdx] {
+			sequenceIdx += 1
+		}
+	}
+	return sequenceIdx == len(sequence)
+}
+
 func main() {
 	array := []int{5, 1, 22, 25, 6, -1, 8, 10}
 	sequence := []int{1, 6, -1, 10}
 	fmt.Printf("First solution's result => %v\n", firstSolution(array, sequence))
+	fmt.Printf("Second solution's result => %v\n", secondSolution(array, sequence))
 }
