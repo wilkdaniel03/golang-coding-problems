f8bb13604ff9c791016225878f1485ca14379546
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-19 22:55:46 +0200

    [Non repeating character] Second solution.

--- non-repeating-character/main.go
+++ non-repeating-character/main.go
@@ -17,7 +17,25 @@ func NSquaredSolution(str string) int {
   return -1
 }
 
+func OptimalSolution(str string) int {
+  chars := map[rune]int{}
+  for _, char := range str {
+    if _, ok := chars[char]; !ok {
+      chars[char] = 1
+    } else {
+      chars[char] += 1
+    }
+  }
+  for idx, char := range str {
+    if chars[char] == 1 {
+      return idx
+    }
+  }
+  return -1
+}
+
 func main() {
   string := "abcdcaf"
   fmt.Printf("First result => %d\n", NSquaredSolution(string))
+  fmt.Printf("Second result => %d\n", OptimalSolution(string))
 }
