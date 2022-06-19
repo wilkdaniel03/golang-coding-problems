6234596323d8243bfbf1a2236b48ba086df9b151
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-19 17:59:08 +0200

    [Palindrome] Third solution.

--- palindrome/main.go
+++ palindrome/main.go
@@ -18,7 +18,22 @@ func AverageIterativeSolution(str string) bool {
   return str == string(charArray)
 }
 
+func AverageRecursiveSolution(str string) bool {
+  return reversiveHelper(str, 0)
+}
+
+func reversiveHelper(str string, i int) bool {
+  if i >= len(str)-1-i {
+    return true
+  }
+  if str[i] != str[len(str)-1-i] {
+    return false
+  }
+  return reversiveHelper(str, i+1)
+}
+
 func main() {
   fmt.Printf("First result => %v\n", WorstSolution("abcdcba"))
   fmt.Printf("Second result => %v\n", AverageIterativeSolution("abcdcba"))
+  fmt.Printf("Third result => %v\n", AverageRecursiveSolution("abcdcba"))
 }
