51181de2933da3fa9a02afc7c4449226b2597517
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-19 18:01:53 +0200

    [Palindrome] Fourth solution.

--- palindrome/main.go
+++ palindrome/main.go
@@ -32,8 +32,18 @@ func reversiveHelper(str string, i int) bool {
   return reversiveHelper(str, i+1)
 }
 
+func OptimalSolution(str string) bool {
+  for startIdx := 0; startIdx < len(str); startIdx++ {
+    if str[startIdx] != str[len(str)-1-startIdx] {
+      return false
+    }
+  }
+  return true
+}
+
 func main() {
   fmt.Printf("First result => %v\n", WorstSolution("abcdcba"))
   fmt.Printf("Second result => %v\n", AverageIterativeSolution("abcdcba"))
   fmt.Printf("Third result => %v\n", AverageRecursiveSolution("abcdcba"))
+  fmt.Printf("Fourth result => %v\n", OptimalSolution("abcdcba"))
 }
