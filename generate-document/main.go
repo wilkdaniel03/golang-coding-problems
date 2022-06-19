ee1bacd392928fd6976c40c2f03609f8abca068f
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-19 22:18:44 +0200

    [Generate document] Second solution.

--- generate-document/main.go
+++ generate-document/main.go
@@ -13,6 +13,22 @@ func NSquaredSolution(characters string, document string) bool {
   return true
 }
 
+func CachedSolution(characters string, document string) bool {
+  counted := map[rune]bool{}
+  for _, c := range document {
+    if _, ok := counted[c]; !ok {
+      expectedFreq := checkCharacter(c, document)
+      freq := checkCharacter(c, characters)
+      if expectedFreq > freq {
+        return false
+      } else {
+        counted[c] = true
+      }
+    }
+  }
+  return true
+}
+
 func checkCharacter(char rune, collection string) int {
   freq := 0
   for _, c := range collection {
@@ -27,4 +43,5 @@ func main() {
   characters := "Bste!hetsi ogEAxpelrt x "
   document := "AlgoExpert is the Best!"
   fmt.Printf("First result => %v\n", NSquaredSolution(characters, document))
+  fmt.Printf("Second result => %v\n", CachedSolution(characters, document))
 }
