72bca9310fded8abe94b0664f6ff325b25e435b3
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-21 17:39:57 +0200

    [Monotonic array] Second solution.

--- monotonic-array/main.go
+++ monotonic-array/main.go
@@ -28,7 +28,21 @@ func checkIfBreaksDirection(direction, current, previous int) bool {
   return difference < 0
 }
 
+func SecondSolution(array []int) bool {
+  isNonDecreasing, isNonIncreasing := true, true
+  for i := 1; i < len(array); i++ {
+    if array[i] < array[i-1] {
+      isNonDecreasing = false
+    }
+    if array[i] > array[i-1] {
+      isNonIncreasing = false
+    }
+  }
+  return isNonDecreasing || isNonIncreasing
+}
+
 func main() {
   array := []int{-1,-5,-10,-1100,-1100,-1101,-1102,-9001}
   fmt.Printf("First result => %v\n", FirstSolution(array))
+  fmt.Printf("Second result => %v\n", SecondSolution(array))
 }
