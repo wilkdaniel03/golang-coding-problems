17a18cd7c3a995e9c0e4ea18352ce73e80dd9941
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-21 23:14:19 +0200

    [Spiral traverse] Recursive solution.

--- spiral-traverse/main.go
+++ spiral-traverse/main.go
@@ -33,6 +33,48 @@ func IterativeSolution(matrix [][]int) []int {
   return result
 }
 
+func RecursiveSolution(matrix [][]int) []int {
+  result := []int{}
+  helper(0, len(matrix)-1, 0, len(matrix[0])-1, matrix, &result)
+  return result
+}
+
+func helper(
+  firstRow, lastRow, firstCol, lastCol int,
+  matrix [][]int,
+  result *[]int,
+) {
+  if firstRow > lastRow || firstCol > lastCol {
+    return
+  }
+  for col := firstCol; col <= lastCol; col++ {
+    *result = append(*result, matrix[firstRow][col])
+  }
+  for row := firstRow+1; row <= lastRow; row++ {
+    *result = append(*result, matrix[row][lastCol])
+  }
+  for col := lastCol-1; col >= firstCol; col-- {
+    if firstRow == lastRow {
+      break
+    }
+    *result = append(*result, matrix[lastRow][col])
+  }
+  for row := lastRow-1; row > firstRow; row-- {
+    if firstCol == lastCol {
+      break
+    }
+    *result = append(*result, matrix[row][firstCol])
+  }
+  helper(
+    firstRow+1,
+    lastRow-1,
+    firstCol+1,
+    lastCol-1,
+    matrix,
+    result,
+    )
+}
+
 func main() {
   matrix := [][]int{
     {1,2,3,4},
@@ -41,4 +83,5 @@ func main() {
     {10,9,8,7},
   }
   fmt.Printf("First result => %v\n", IterativeSolution(matrix))
+  fmt.Printf("Second result => %v\n", RecursiveSolution(matrix))
 }
